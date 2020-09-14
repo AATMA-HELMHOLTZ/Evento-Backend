02b6943a601e1ae701b21011f6ca72bb119b1493
MeanMachiNe13 <e19cse107@bennett.edu.in>
2020-09-14 10:05:41 +0530

    Added User Schema to db with minimal details, created unfactored app.js for testing auth functions

--- app.js
+++ app.js
@@ -1,1 +1,92 @@
+var express                 = require("express");
+var app                     = express();
+var bodyParser              = require("body-parser");
+var mongoose                = require("mongoose");
+var User                    = require("./models/user");
+var passport                = require("passport");
+var passportLocalMongoose   = require("passport-local-mongoose");
+var LocalStrategy           = require("passport-local");
 
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(__dirname + "/public"));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(require("express-session")({
+    secret: "AATMA is GOAT", 
+    resave: false, 
+    saveUninitialized: false
+}));
+
+
+mongoose.connect('mongodb://localhost:27017/demo_auth_v1', {
+    useNewUrlParser: true, 
+    useUnifiedTopology: true
+})
+.then(() => console.log("connected to db"))
+.catch(() => console.log(error.message));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+passport.use(new LocalStrategy(User.authenticate()));
+
+//------------------ROUTES----------------
+
+//APP ROUTES
+app.get("/", function(req, res){
+    //INCLUDE FRONTEND
+});
+
+
+//show signup from
+app.get("/register", function(req, res){
+    //INCLUDE FRONTEND
+});
+
+app.post("/register", function(req, res){
+    // req.body.username
+    // req.body.password
+    User.register(new User({username: req.body.username}), req.body.password, function (err, user){
+        if (err) {
+            console.log(err);
+            //INCLUDE REGISTER FROM
+        } else {
+            passport.authenticate("local")(req, res, function(){
+                //REDIRECT 
+            });
+        }
+    })
+});
+
+//login routes
+
+app.get("/login", function(req, res){
+    //INCLUDE FRONTEND FOR LOGIN
+});
+
+
+app.post("/login", passport.authenticate("local",{
+    successRedirect: "/", 
+    failureRedirect: "/login"
+}), function(req, res){  
+});
+
+app.get("/logout", function(req, res){
+    req.logout();
+    res.redirect("/");
+});
+
+
+//MIDDLEWARE
+function isLoggedIn(req, res, next){
+    if(req.isAuthenticated()){
+         next();
+    }
+    res.redirect("/login");
+}
+
+//SERVER INITIALISE 
+//============================================
+var port = process.env.PORT || 3000;
+app.listen(port, function(){
+    console.log("Started server");
+});
